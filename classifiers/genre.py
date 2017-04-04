42ff8ca78f49d2b4a35669518592c37086323b60
Joseph DeSimpliciis <jcdesimp@gmail.com>
2017-04-03 22:43:40 -0400

    Implement VERY basic/bad (possibly broken) initial genre classifier.

--- classifiers/genre.py
+++ classifiers/genre.py
@@ -1,2 +1,76 @@
+from sklearn.feature_extraction import DictVectorizer
+from collections import defaultdict
+import numpy
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import LinearSVC
+from sklearn.externals import joblib
+import os.path
+
 # implement binary classification
 GENRES = ['GENRE_A', 'GENRE_B']
+MODEL_FILE = 'trained_models/genre_model.pkl'
+FEAT_VECTOR_FILE = 'trained_models/genre_feat_vector.pkl'
+
+def extract_features(trainingData):
+  """Extract and tag features on the training data
+  Assumes tokenization and pos tagging has been done"""
+  feature_list = []
+  for data_entry in trainingData:
+    entry_feature_dict = defaultdict(lambda : 0)
+    # entry_feature_dict = dict()
+    # entry_feature_dict["token_count"] = len(data_entry["tokens"])
+    for t in data_entry["tokens"]:
+      # feature : does text contain word
+      entry_feature_dict["has_word (" + t.lower() + ")"] = 1
+      # entry_feature_dict["wc (" + t.lower() + ")"] += 1
+      if t.lower() == 'i':
+        entry_feature_dict["contains I"] += 1
+    feature_list.append(entry_feature_dict)
+  return feature_list
+
+def extract_labels(trainingData):
+  """product an array of truth labels for the genres"""
+  return [y["truth"]["genre"] for y in trainingData]
+
+def train_model(x, y):
+  """produce a trained classifier model
+  x : the feature list
+  y : the label list
+  """
+  feat_vector = DictVectorizer().fit(x)
+  X_train = feat_vector.transform(x)
+
+  classifier = LinearSVC().fit(X_train, y)
+
+  # save the trained model to disk
+  joblib.dump(classifier, MODEL_FILE)
+  # save the fitted feature vector
+  joblib.dump(feat_vector, FEAT_VECTOR_FILE) 
+
+def test_model(x, y):
+  """test the classifier model
+  x : the feature list
+  y : the label list
+  """
+
+  # check if trained model exists
+  if not os.path.isfile(MODEL_FILE) or not os.path.isfile(FEAT_VECTOR_FILE):
+    print('Genre model "' + MODEL_FILE +'" not found.')
+    print('be sure to train model first!')
+    return 
+
+  feat_vector = joblib.load(FEAT_VECTOR_FILE)
+  X_test = feat_vector.transform(x)
+
+  classifier = joblib.load(MODEL_FILE)
+
+  test_pred = classifier.predict(X_test)
+  # acc = numpy.mean([xi==yi for xi in test_pred for yi in y])
+  # f1_fp_count = len([xi==yi for xi in test_pred for yi in y])
+  # print("Accuracy: " + str(acc))
+  # print(test_pred)
+  return test_pred
+
+
+
+
--- main.py
+++ main.py
@@ -6,12 +6,11 @@ import sklearn
 from nltk.tokenize import word_tokenize
 from nltk import pos_tag
 
-from classifiers import genre, polarity, eventType
+from classifiers import genre, polarity, event_type
 
 def parseDataFile(filename):
   """Parse data from file"""
-  print("parsing data from file...")
-  parsedData = []
+  # print("parsing data from file...")
   # parsed data will be a list of
   # dictionaries in the format:
   # 
@@ -25,6 +24,7 @@ def parseDataFile(filename):
   #   }
   # }
   #
+  parsedData = []
   openFile = open(filename)
   for line in openFile:
     line = line.strip()
@@ -43,14 +43,14 @@ def parseDataFile(filename):
 
 def tokenizeText(parsedData):
   """Tokenize text and add "tokens" property to parsed data"""
-  print("tokenizing...")
+  # print("tokenizing...")
   for d in parsedData:
     d["tokens"] = word_tokenize(d["text"])
 
 def tagPOS(parsedData):
   """Generate POS tags for tokens and set "pos_tags" property on parsed data
   assumes tokenzation has been done and as on the 'tokens' property of the parsedData entries"""
-  print("tagging POSs...")
+  # print("tagging POSs...")
   for d in parsedData:
     d["pos_tags"] = pos_tag(d["tokens"])
 
@@ -61,16 +61,46 @@ def main():
   parser.add_argument('--train', required=False, metavar='TRAINING_DATA', help='Train the classifiers with the given training data file.')
   parser.add_argument('--test', required=False, metavar='TEST_DATA', help='Test the classifiers with the given training data file.')
   args = parser.parse_args()
+
   if args.train:
     parsedTrainingData = parseDataFile(args.train)
     # attach tokens
     tokenizeText(parsedTrainingData)
     # attach POS tags
     tagPOS(parsedTrainingData)
-    # print(parsedTrainingData)
-    
+
+    # extract features for genre classifier
+    genre_features = genre.extract_features(parsedTrainingData)
+    genre_labels = genre.extract_labels(parsedTrainingData)
+
+    genre.train_model(genre_features, genre_labels)
+
+
   elif args.test:
-    print("test: not implemented")
+
+    parsedTestData = parseDataFile(args.test)
+    # attach tokens
+    tokenizeText(parsedTestData)
+    # attach POS tags
+    tagPOS(parsedTestData)
+    # extract features for genre classifier
+    genre_features = genre.extract_features(parsedTestData)
+    genre_labels = genre.extract_labels(parsedTestData)
+
+    genre_predictions = genre.test_model(genre_features, genre_labels)
+
+    predictions = []
+    for i, e in enumerate(parsedTestData):
+      single_result = []
+      single_result.append(e["id"])
+      single_result.append(e["text"])
+      single_result.append(e["truth"]["polarity"]) # print out TRUTH for now
+      single_result.append(e["truth"]["topic"]) # print out TRUTH for now
+      single_result.append(genre_predictions[i])
+      
+      predictions.append("\t".join(single_result) + "\t")
+    print("\n".join(predictions))
+      
   else:
     parser.print_help()
 
